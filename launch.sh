508749544d00793f7a96f4e2dab71fffb6c3e8c6
xxx44552 <xxx44552@gmail.com>
2022-03-06 21:56:06 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y && sudo apt install docker.io -y && sudo apt install iptraf -y && sudo apt install speedtest-cli -y
+
 for (( c=$1; c<=$2; c++ ))
 do  
    sudo docker run --restart=always --name=db1000n$((c)) --detach --pull=always ghcr.io/arriven/db1000n:latest
