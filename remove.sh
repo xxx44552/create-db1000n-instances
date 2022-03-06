2dd28a6eb51275341a23e1c90cac635da03ff7d0
xxx44552 <xxx44552@gmail.com>
2022-03-07 00:29:26 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y && sudo apt install docker.io -y
 for (( c=$1; c<=$2; c++ ))
 do  
    docker rm $(docker stop $(docker ps -a -q --filter ancestor=db1000n$((c)) --format="{{.ID}}"))
