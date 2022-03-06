11495371ae7ff779dd0e582e3e86a6487d0d397e
xxx44552 <xxx44552@gmail.com>
2022-03-07 00:48:03 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for (( c=$1; c<=$2; c++ ))
 do  
-   sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter name=db1000n$((c)) --format="{{.ID}}"))
+   sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter name=db1000n$((c))))
    echo "Removed db1000n$c container"
 done
