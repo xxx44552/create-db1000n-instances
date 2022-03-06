40269ed52b971d5f1c08b064343cbd343d05f864
xxx44552 <xxx44552@gmail.com>
2022-03-06 21:40:20 +0200

    log

--- launch.sh
+++ launch.sh
@@ -3,5 +3,5 @@ sudo apt update && sudo apt upgrade -y && sudo apt install docker.io -y
 for (( c=$1; c<=$2; c++ ))
 do  
    sudo docker run --restart=always --name=db1000n$((c)) --detach --pull=always ghcr.io/arriven/db1000n:latest
-   echo "Started db1000n$c docker"
+   echo "Started db1000n$c docker <------------"
 done
